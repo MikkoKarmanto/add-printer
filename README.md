6555a30ced13c21559a5cc7bf34dc7dae4c8db7d
Karm <mikko.karmanto@gmail.com>
2018-01-11 00:16:49 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+Add Printer.exe is simple program that, installs and confiqures printer queue to be used with Muti Function Devices (MFP) and/or with print management systems. e.g Y-Soft SafeQ. This program was writen to ease deployment of printer queues to client computers, that cannot utilize print servers for deployment.
+
+This program is built for KONICA MINOLTA MFP devices and Y-Soft SafeQ. No other vendor devices or software have been tested.
+
+
+Requirements
+Printer driver with .inf config files is used to install driver packages to computer. No other methods are currently supported.
+
+
+Supported Operating systems:
+- Windows 10 (x64)
+
+File structure:
+    (Base folder)
+    |----- .\Driver\x64\(64 bit driver files goes here).
+    |----- .\Driver\x86\(32 bit driver files goes here).
+    |- settings.ini
+    |- LICENSE.txt
+    |- README.md
+
+
+https://github.com/MikkoKarmanto
---
+++ main.cpp
@@ -0,0 +1,114 @@
+// Example that shows simple usage of the INIReader class
+
+#include <iostream>
+#include <sstream>
+#include "INIReader.h"
+#include <cstdlib>
+#include <vector>
+#include <windows.h>
+
+using namespace std;
+
+/*string sections(INIReader &reader)
+{
+    stringstream ss;
+    set<string> sections = reader.Sections();
+    for (set<string>::iterator it = sections.begin(); it != sections.end(); ++it)
+        ss << *it << ",";
+    return ss.str();
+}
+*/
+typedef BOOL (WINAPI *LPFN_ISWOW64PROCESS) (HANDLE, PBOOL);
+
+LPFN_ISWOW64PROCESS fnIsWow64Process;
+
+BOOL IsWow64()
+{
+    BOOL bIsWow64 = FALSE;
+
+    //IsWow64Process is not available on all supported versions of Windows.
+    //Use GetModuleHandle to get a handle to the DLL that contains the function
+    //and GetProcAddress to get a pointer to the function if available.
+
+    fnIsWow64Process = (LPFN_ISWOW64PROCESS) GetProcAddress(
+        GetModuleHandle(TEXT("kernel32")),"IsWow64Process");
+
+    if(NULL != fnIsWow64Process)
+    {
+        if (!fnIsWow64Process(GetCurrentProcess(),&bIsWow64))
+        {
+            //handle error
+        }
+    }
+    return bIsWow64;
+}
+
+int main()
+{
+    INIReader reader("settings.ini");
+
+    if (reader.ParseError() < 0) {
+        cout << "Can't load 'Settings.ini'" << endl;
+        return 1;
+    }
+    vector<string> vect;
+    string DriverInfLoc, PortNames, PortNameAndNumber;
+
+    if(IsWow64()) {
+        cout << "64 bit" << endl;
+        DriverInfLoc = reader.Get("Driver", "DriverInfLoc_x64", "UNKNOWN");
+    }
+    else {
+        cout << "32 bit" << endl;
+        DriverInfLoc = reader.Get("Driver", "DriverInfLoc_x86", "UNKNOWN");
+    }
+
+    string ServerIP = reader.Get("Server", "serverIP", "UNKNOWN");
+    string DriverName = reader.Get("Driver", "DriverName", "UNKNOWN");
+    string PrinterName = reader.Get("Queue", "PrinterName", "UNKNOWN");
+    string PortName = reader.Get("Queue", "PortName", "UNKNOWN");
+    string LPRName = reader.Get("Queue", "LPRName", "UNKNOWN");
+
+    stringstream ss(ServerIP);
+    while (ss >> ServerIP)
+    {
+        vect.push_back(ServerIP);
+
+        if (ss.peek() == ',')
+            ss.ignore();
+    }
+
+
+    for (size_t i=0; i< vect.size(); i++) {
+        PortNameAndNumber = PortName + "_" + to_string(i + 1);
+        string create_port = "Cscript c:\\windows\\system32\\printing_admin_scripts\\en-us\\Prnport.vbs -a -r " + PortNameAndNumber + " -h " + vect.at(i) + " -o lpr -q " + LPRName;
+        const char *cCreate_port = create_port.c_str();
+        system(cCreate_port);
+        if(i > 0 && i < vect.size()){
+            PortNames += "," + PortNameAndNumber;
+        }
+        else{
+            PortNames = PortNameAndNumber;
+        }
+    }
+
+    string install_driver = "rundll32 printui.dll,PrintUIEntry /ia /f " + DriverInfLoc + " /m " + DriverName ;
+    string install_printer = "rundll32 printui.dll,PrintUIEntry /if /b " + PrinterName + " /f " + DriverInfLoc + " /r " + PortNameAndNumber + " /m " + DriverName;
+    string add_ports_to_queue = "rundll32 printui.dll,PrintUIEntry /Xs /n " + PrinterName + " PortName " + PortNames + " attributes -EnableBidi";
+
+    const char *cInstall_driver = install_driver.c_str();
+    const char *cInstall_printer = install_printer.c_str();
+    const char *cAdd_ports_to_queue = add_ports_to_queue.c_str();
+
+    cout << cInstall_driver << endl;
+	system(cInstall_driver);
+	cout << cInstall_printer << endl;
+	system(cInstall_printer);
+    cout << cAdd_ports_to_queue << endl;
+	system(cAdd_ports_to_queue);
+
+
+    return 0;
+}
+
+
