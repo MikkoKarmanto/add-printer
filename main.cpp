e6aa1a427ba458d3def2dca9e3184fd56495552a
Karm <mikko.karmanto@gmail.com>
2018-01-23 07:05:56 +0200

    Added IP posibility to add network printers IP to get settings from. Removed command prompt window opening.

--- main.cpp
+++ main.cpp
@@ -7,6 +7,7 @@
 #include <vector>
 #include <windows.h>
 
+
 using namespace std;
 
 typedef BOOL (WINAPI *LPFN_ISWOW64PROCESS) (HANDLE, PBOOL);
@@ -43,22 +44,21 @@ int main()
         return 1;
     }
     vector<string> vect;
-    string DriverInfLoc, PortNames, PortNameAndNumber;
+    string DriverInfLoc, PortNames, PortNameAndNumber, install_printer;
 
     if(IsWow64()) {
-        cout << "64 bit" << endl;
         DriverInfLoc = reader.Get("Driver", "DriverInfLoc_x64", "UNKNOWN");
     }
     else {
-        cout << "32 bit" << endl;
         DriverInfLoc = reader.Get("Driver", "DriverInfLoc_x86", "UNKNOWN");
     }
 
-    string ServerIP = reader.Get("Server", "serverIP", "UNKNOWN");
+    string ServerIP = reader.Get("Server", "serverIP", "127.0.0.1");
     string DriverName = reader.Get("Driver", "DriverName", "UNKNOWN");
     string PrinterName = reader.Get("Queue", "PrinterName", "UNKNOWN");
     string PortName = reader.Get("Queue", "PortName", "UNKNOWN");
     string LPRName = reader.Get("Queue", "LPRName", "UNKNOWN");
+    string IP = reader.Get("Printer", "IP", "UNKNOWN");
 
     stringstream ss(ServerIP);
     while (ss >> ServerIP)
@@ -68,7 +68,6 @@ int main()
         if (ss.peek() == ',')
             ss.ignore();
     }
-
 
     for (size_t i=0; i< vect.size(); i++) {
         PortNameAndNumber = PortName + "_" + to_string(i + 1);
@@ -82,23 +81,26 @@ int main()
             PortNames = PortNameAndNumber;
         }
     }
+    if (IP != "UNKNOWN" && IP.size() > 0 ) {
+        string create_port_1 = "Cscript .\\scripts\\prnport.vbs -a -r " + IP +" -h " + IP +" -o raw -n 9100";
+        const char *cCreate_port_1 = create_port_1.c_str();
+        system(cCreate_port_1);
+        install_printer = "rundll32 printui.dll,PrintUIEntry /if /b " + PrinterName + " /f " + DriverInfLoc + " /r " + IP + " /m " + DriverName;
+    }
+    else {
+        install_printer = "rundll32 printui.dll,PrintUIEntry /if /b " + PrinterName + " /f " + DriverInfLoc + " /r " + PortNameAndNumber + " /m " + DriverName;
+    }
 
     string install_driver = "rundll32 printui.dll,PrintUIEntry /ia /f " + DriverInfLoc + " /m " + DriverName ;
-    string install_printer = "rundll32 printui.dll,PrintUIEntry /if /b " + PrinterName + " /f " + DriverInfLoc + " /r " + PortNameAndNumber + " /m " + DriverName;
     string add_ports_to_queue = "rundll32 printui.dll,PrintUIEntry /Xs /n " + PrinterName + " PortName " + PortNames + " attributes -EnableBidi";
 
     const char *cInstall_driver = install_driver.c_str();
     const char *cInstall_printer = install_printer.c_str();
     const char *cAdd_ports_to_queue = add_ports_to_queue.c_str();
 
-
-    cout << "Installing driver " + DriverName << endl;
 	system(cInstall_driver);
-	cout << "Installing printer queue " + PrinterName << endl;
 	system(cInstall_printer);
-    cout << "Configuring printer..."  << endl;
 	system(cAdd_ports_to_queue);
-
 
     return 0;
 }
