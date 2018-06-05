2f838e66b0fe6cf71390ad7f3b1eb718392bf460
Karm <mikko.karmanto@gmail.com>
2018-06-06 00:32:44 +0300

    Added option to use command line arguments

--- main.cpp
+++ main.cpp
@@ -1,21 +1,17 @@
-// Example that shows simple usage of the INIReader class
-
 #include <iostream>
 #include <sstream>
 #include "INIReader.h"
 #include <cstdlib>
 #include <vector>
 #include <windows.h>
-
-
-using namespace std;
+#include <algorithm>
+#include <string>
 
 typedef BOOL (WINAPI *LPFN_ISWOW64PROCESS) (HANDLE, PBOOL);
 
 LPFN_ISWOW64PROCESS fnIsWow64Process;
 
-BOOL IsWow64()
-{
+BOOL IsWow64(){
     BOOL bIsWow64 = FALSE;
 
     //IsWow64Process is not available on all supported versions of Windows.
@@ -25,27 +21,107 @@ BOOL IsWow64()
     fnIsWow64Process = (LPFN_ISWOW64PROCESS) GetProcAddress(
         GetModuleHandle(TEXT("kernel32")),"IsWow64Process");
 
-    if(NULL != fnIsWow64Process)
-    {
-        if (!fnIsWow64Process(GetCurrentProcess(),&bIsWow64))
-        {
+    if(NULL != fnIsWow64Process){
+        if (!fnIsWow64Process(GetCurrentProcess(),&bIsWow64)){
             //handle error
         }
     }
     return bIsWow64;
 }
 
-int main()
-{
-    INIReader reader("settings.ini");
+char* getCmdOption(char ** begin, char ** end, const std::string & option){
+    char ** itr = std::find(begin, end, option);
+    if (itr != end && ++itr != end)
+    {
+        return *itr;
+    }
+    return 0;
+}
 
-    if (reader.ParseError() < 0) {
-        cout << "Can't load 'Settings.ini'" << endl;
+bool cmdOptionExists(char** begin, char** end, const std::string& option){
+    return std::find(begin, end, option) != end;
+}
+
+int main(int argc, char* argv[]){
+    INIReader reader("settings.ini");
+    // Check if ini file found or command line argument where passed. 
+    if (reader.ParseError() < 0){
+        std::cout << "No ini file found" << std::endl;
         return 1;
     }
-    vector<string> vect;
-    string DriverInfLoc, PortNames, PortNameAndNumber, install_printer;
+    std::vector<string> vect;
+    string DriverInfLoc, PortNames, PortNameAndNumber, install_printer, ServerIP, DriverName, PrinterName;
+    string PortName, LPRName, IP;
 
+    if(cmdOptionExists(argv, argv+argc, "-HELP") 
+        || cmdOptionExists(argv, argv+argc, "-?") 
+        || cmdOptionExists(argv, argv+argc, "-help"))
+    {
+        std::cout << "Usage: addprinter [-HELP] [-IP server_ip][-DRIVER driver][-NAME name][-PORTNAME name][-LPR name][-PRINTER ip]" << std::endl;
+        std::cout <<  "Arguments:" << std::endl;
+        std::cout <<  "-IP           - IP address of the server" << std::endl;
+        std::cout <<  "-LPR          - queue name, applies to TCP LPR ports only" << std::endl;
+        std::cout <<  "-PORTNAME     - port name" << std::endl;
+        std::cout <<  "-DRIVER       - Driver name to be installed, have to match diver name in .INF file" << std::endl;
+        std::cout <<  "-NAME         - Print queue name" << std::endl;
+        std::cout <<  "-PRINTER      - ip of a printer to get settings from" << std::endl;
+        std::cout <<  "The last example will try to get the device settings at the specified IP address." << std::endl;
+        std::cout <<  "If a device is detected, then a TCP port is added with the preferred settings for that device." << std::endl;
+        return 1;
+    }
+
+    char * option = getCmdOption(argv, argv + argc, "-IP");
+    if (option)
+    {
+        ServerIP = option;
+    }
+    option = getCmdOption(argv, argv + argc, "-DRIVER");
+    if (option)
+    {
+        DriverName = option;
+    }
+    option = getCmdOption(argv, argv + argc, "-NAME");
+    if (option)
+    {
+        PrinterName = option;
+    }
+    option = getCmdOption(argv, argv + argc, "-PORTNAME");
+    if (option)
+    {
+        PortName = option;
+    }
+    option = getCmdOption(argv, argv + argc, "-LPR");
+    if (option)
+    {
+        LPRName = option;
+    }
+    option = getCmdOption(argv, argv + argc, "-PRINTER");
+    if (option)
+    {
+        IP = option;
+    }
+
+    // Read settings from ini file if parameters are not given.
+    if (ServerIP.length() < 1){
+        ServerIP = reader.Get("Server", "serverIP", "127.0.0.1");
+    } 
+    if (DriverName.length() < 1){
+        DriverName = reader.Get("Driver", "DriverName", "UNKNOWN");
+    }
+    if (PrinterName.length() < 1){
+        PrinterName = reader.Get("Queue", "PrinterName", "UNKNOWN");
+    }
+    if (PortName.length() < 1){
+        PortName = reader.Get("Queue", "PortName", "UNKNOWN");
+    }
+    if (LPRName.length() < 1){
+        LPRName = reader.Get("Queue", "LPRName", "UNKNOWN");
+    }
+    if (IP.length() < 1){
+        IP = reader.Get("Printer", "IP", "UNKNOWN");
+    }
+
+    // Check CPU architecture and load correct drivers.
     if(IsWow64()) {
         DriverInfLoc = reader.Get("Driver", "DriverInfLoc_x64", "UNKNOWN");
     }
@@ -53,14 +129,7 @@ int main()
         DriverInfLoc = reader.Get("Driver", "DriverInfLoc_x86", "UNKNOWN");
     }
 
-    string ServerIP = reader.Get("Server", "serverIP", "127.0.0.1");
-    string DriverName = reader.Get("Driver", "DriverName", "UNKNOWN");
-    string PrinterName = reader.Get("Queue", "PrinterName", "UNKNOWN");
-    string PortName = reader.Get("Queue", "PortName", "UNKNOWN");
-    string LPRName = reader.Get("Queue", "LPRName", "UNKNOWN");
-    string IP = reader.Get("Printer", "IP", "UNKNOWN");
-
-    stringstream ss(ServerIP);
+    std::stringstream ss(ServerIP);
     while (ss >> ServerIP)
     {
         vect.push_back(ServerIP);
@@ -70,7 +139,7 @@ int main()
     }
 
     for (size_t i=0; i< vect.size(); i++) {
-        PortNameAndNumber = PortName + "_" + to_string(i + 1);
+        PortNameAndNumber = PortName + "_" + std::to_string(i + 1);
         string create_port = "Cscript .\\scripts\\Prnport.vbs -a -r " + PortNameAndNumber + " -h " + vect.at(i) + " -o lpr -q " + LPRName;
         const char *cCreate_port = create_port.c_str();
         system(cCreate_port);
